47d49d024cc46ec39b1ac65dd7f1398fc34db2d2
Lucas <dants.dev@gmail.com>
2024-01-16 16:10:37 -0300

    tests from EmailQueue

--- src/controllers/EmailController.spec.js
+++ src/controllers/EmailController.spec.js
@@ -1,17 +1,19 @@
-
 // importar a função do sendEmail controller
-const EmailController = require('./EmailController');
+const EmailController = require("./EmailController");
+const EmailQueue = require("../queue/MailQueue");
 
-describe('Email Controller', () => {
-    test('should sent email sucessfuly', () => {
-        const request = {
-            body: {
-                email: "teste@teste.com",
-                firstName: "Lucas",
-                lastName: "Dantas"
-            }
-        }
-        /*utilizando mockup
+jest.mock("../queue/MailQueue.js");
+
+describe("Email Controller", () => {
+  test("should sent email sucessfuly", async () => {
+    const request = {
+      body: {
+        email: "teste@teste.com",
+        firstName: "Lucas",
+        lastName: "Dantas",
+      },
+    };
+    /*utilizando mockup
 
         "return reply.code(200).send();"
 
@@ -19,11 +21,33 @@ describe('Email Controller', () => {
         utilizar o mockReturnThis para retornar o proprio objeto 
         assim ele tem acesso ao metodo send */
 
-        const reply = {
-            code: jest.fn().mockReturnThis(), 
-            send: jest.fn()
-        }
+    const reply = {
+      code: jest.fn().mockReturnThis(),
+      send: jest.fn(),
+    };
 
-        EmailController.sendEmail(request, reply)   
-    });
+    const template = `
+    Olá ${request.body.firstName} ${request.body.lastName}, sua assinatura foi confirmada!
+    Para acessar seus recursos exclusivos você precisa basta clicar aqui.
+`;
+
+    // invoke
+    await EmailController.sendEmail(request, reply);
+
+    // após o chamar a função, precisamos fazer as assersoes com expect
+    // Esperamos que a função MailQueue seja chamada e add um novo email para ser disparado
+    // E que o controller retorne code status 200
+
+    // Verificar se EmailQueue foi chamado apenas 1 vez
+    expect(EmailQueue.add).toHaveBeenCalledTimes(1);
+    // Verificar se EmailQueue esta adicionando os parâmetros corretos
+   /* expect(EmailQueue.add).toHaveBeenCalledWith({
+      to: "teste@teste.com",
+      from: process.env.EMAIL_FROM,
+      subject: "Assinatura Confrmada",
+      text: template,
+    });*/
+
+    expect(reply.code).toHaveBeenCalledWith(200);
+  });
 });
